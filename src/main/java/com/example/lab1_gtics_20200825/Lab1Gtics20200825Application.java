daaf8779e57b1ec46bdca1f384d78299a8b58db7
= <=>
2023-08-24 20:40:04 -0500

    Aca inicia la creación del proyecto

---
+++ src/main/java/com/example/lab1_gtics_20200825/Lab1Gtics20200825Application.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20200825;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Gtics20200825Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Gtics20200825Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab1_gtics_20200825/Lab1Gtics20200825ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_gtics_20200825;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Gtics20200825ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
